f007631a510e33abfce5aec31fe5a07c789d2e88
Sri Siddhardha Kurra <113198317+srisiddhardhakurra26@users.noreply.github.com>
2023-09-27 16:46:46 -0400

    Time: 714 ms (53.84%), Space: 16.2 MB (97%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -5,12 +5,22 @@ class Solution:
         total = 0
         res = []
         for i in range(len(nums) - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
             for j in range(i + 1, len(nums) - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
                 l, r = j + 1, len(nums) - 1
                 while l < r:
                     total = nums[i] + nums[j] + nums[l] + nums[r]
-                    if total == target and [nums[i], nums[j], nums[l], nums[r]] not in res:
+                    if total == target:
                         res.append([nums[i], nums[j], nums[l], nums[r]])
+                        while l < r and nums[l] == nums[l + 1]:
+                            l += 1
+                        while l < r and nums[r] == nums[r - 1]:
+                            r -= 1
                         l += 1
                         r -= 1
                     elif total < target:
