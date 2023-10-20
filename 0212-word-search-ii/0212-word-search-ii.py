42d1000a76b968a6f319747fa7c551be1576a1d0
Sri Siddhardha Kurra <113198317+srisiddhardhakurra26@users.noreply.github.com>
2023-10-20 11:49:10 -0400

    Time: 1060 ms (78.24%), Space: 21.1 MB (10.64%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,62 +1,55 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+        self.ref = 0
+    
+    def remove(self, word):
+        cur = self
+        cur.ref -= 1
+        for w in word:
+            cur = cur.children[w]
+            cur.ref -= 1
+    
+    def addWord(self, word):
+        cur = self
+        cur.ref += 1
+        for w in word:
+            if w not in cur.children:
+                cur.children[w] = TrieNode()
+            cur = cur.children[w]
+            cur.ref += 1
+        cur.end = True
+
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        m = len(board)
-        n = len(board[0])
-        res = []
+        root = TrieNode()
+        for w in words:
+            root.addWord(w)
+        
+        visit, res = set(), []
+        rows, cols = len(board), len(board[0])
 
-        d = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-
-        ref = set()
-        for i in range(m):
-            for j in range(n-1):
-                ref.add(board[i][j] + board[i][j+1])
-        for j in range(n):
-            for i in range(m-1):
-                ref.add(board[i][j] + board[i+1][j])
-
-        for word in words:
-            f = True
-            for i in range(len(word)-1):
-                if word[i:i+2] not in ref and word[i+1] + word[i] not in ref:
-                    f = False
-                    break
-            if not f:
-                continue
-            if self.findWord(word, m, n, board, d):
+        def dfs(r, c, node, word):
+            if (r < 0 or c < 0 or r == rows or c == cols or board[r][c] not in node.children or (r, c) in visit or node.children[board[r][c]].ref < 1):
+                return 
+            visit.add((r, c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            
+            if node.end:
                 res.append(word)
+                node.end = False
+                root.remove(word)
+            
+            dfs(r + 1, c, node, word)
+            dfs(r - 1, c, node, word)
+            dfs(r, c + 1, node, word)
+            dfs(r, c - 1, node, word)
+
+            visit.remove((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                dfs(r, c, root, "")
         return res
-    
-    def findWord(self, word, m, n, board, d) -> bool:
-        if word[:4] == word[0] * 4:
-            word = ''.join([c for c in reversed(word)])
-        starts = []
-        stack = []
-        visited = set()
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] == word[0]:
-                    if len(word) == 1:
-                        return True
-                    starts.append((i, j))
-        for start in starts:
-            stack.append(start)
-            visited.add((start, ))
-            l = 1
-            while stack != [] and l < len(word):
-                x, y = stack[-1]
-                for dxy in d:
-                    nx, ny = x + dxy[0], y + dxy[1]
-                    if 0 <= nx < m and 0 <= ny < n:
-                        if board[nx][ny] == word[l]:
-                            if (nx, ny) not in stack and tuple(stack) + ((nx, ny),) not in visited:
-                                stack.append((nx, ny))
-                                visited.add(tuple(stack))
-                                l += 1
-                                if l == len(word):
-                                    return True
-                                break
-                else:
-                    stack.pop()
-                    l -= 1
-        else:
-            return False
