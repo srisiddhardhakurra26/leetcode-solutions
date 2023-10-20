77de765d04b8483b63cf959d35f519de55bbbda2
Sri Siddhardha Kurra <113198317+srisiddhardhakurra26@users.noreply.github.com>
2023-10-20 16:19:34 -0400

    Time: 62 ms (52.04%), Space: 16.6 MB (43.01%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,23 +1,19 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        res, subres, hm = [], [], {}
+        res = []
+        nums.sort()
 
-        for n in nums:
-            hm[n] = 1 + hm.get(n, 0)
-        
-        def dfs():
-            if len(subres) == len(nums):
+        def dfs(subres, remaining_nums):
+            if not remaining_nums:
                 res.append(subres.copy())
                 return
-            
-            for n in hm:
-                if hm[n] > 0:
-                    subres.append(n)
-                    hm[n] -= 1
-                    
-                    dfs()
 
-                    subres.pop()
-                    hm[n] += 1
-        dfs()
+            for i in range(len(remaining_nums)):
+                if i > 0 and remaining_nums[i] == remaining_nums[i - 1]:
+                    continue
+                subres.append(remaining_nums[i])
+                dfs(subres, remaining_nums[:i] + remaining_nums[i+1:])
+                subres.pop()
+
+        dfs([], nums)
         return res
