c758af5873b068482bd2f87bac05e4fbe86be0ee
Sri Siddhardha Kurra <113198317+srisiddhardhakurra26@users.noreply.github.com>
2023-10-20 21:31:27 -0400

    Time: 36 ms (72.76%), Space: 16.3 MB (66.8%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,16 +1,20 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        res = []
+        res, curStr = [], ""
         hm = {"2" : "abc", "3" : "def", "4" : "ghi", "5" : "jkl", "6" : "mno", "7" : "pqrs", "8" : "tuv", "9" : "wxyz"}
 
-        def dfs(i, curStr):
+        def dfs(i):
+            nonlocal curStr
             if len(digits) == len(curStr):
                 res.append(curStr[:])
                 return
 
             for j in hm[digits[i]]:
-                dfs(i + 1, curStr + j)
+                curStr += j
+                dfs(i + 1)
+                curStr = curStr[:-1]
 
 
-        if digits: dfs(0, "")
-        return res 
+        dfs(0)
+        if digits:
+            return res
