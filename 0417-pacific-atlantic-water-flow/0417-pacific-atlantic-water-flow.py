e155bf7b60387e1bfa7bfd266e7fd1edbf7114f4
Sri Siddhardha Kurra <113198317+srisiddhardhakurra26@users.noreply.github.com>
2023-10-24 11:41:45 -0400

    Time: 233 ms (85.62%), Space: 18 MB (41.13%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,25 +1,24 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pacific, atlantic = set(), set()
+        pac, atl = set(), set()
         rows, cols = len(heights), len(heights[0])
-
-        def dfs(r, c, visit, prevHigh):
-            if r < 0 or c < 0 or r == rows or c == cols or (r, c) in visit or heights[r][c] < prevHigh:
+        
+        def dfs(r, c, visit, prevHeight):
+            if r < 0 or c < 0 or r == rows or c == cols or (r, c) in visit or heights[r][c] < prevHeight:
                 return
-
+            
             visit.add((r, c))
-            prevHigh = heights[r][c]
-            dfs(r + 1, c, visit, prevHigh)
-            dfs(r - 1, c, visit, prevHigh)
-            dfs(r, c + 1, visit, prevHigh)
-            dfs(r, c - 1, visit, prevHigh)
-        
-        for r in range(rows):
-            dfs(r, 0, pacific, 0)
-            dfs(r, cols - 1, atlantic, 0)
-        for c in range(cols):
-            dfs(0, c, pacific, 0)
-            dfs(rows - 1, c, atlantic, 0)
-        return list(pacific & atlantic)
+            prevHeight = heights[r][c]
+            dfs(r + 1, c, visit, prevHeight)
+            dfs(r - 1, c, visit, prevHeight)
+            dfs(r, c + 1, visit, prevHeight)
+            dfs(r, c - 1, visit, prevHeight)
 
-        
+
+        for r in range(rows):
+            dfs(r, 0, pac, 0)
+            dfs(r, cols - 1, atl, 0)
+        for c in range(cols):
+            dfs(0, c, pac, 0)
+            dfs(rows - 1, c, atl, 0)
+        return list(atl & pac)
