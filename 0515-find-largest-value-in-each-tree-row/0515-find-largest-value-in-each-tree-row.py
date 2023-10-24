f6067c8e364acbfb8f468e9f62d2a21eaecdcd26
Sri Siddhardha Kurra <113198317+srisiddhardhakurra26@users.noreply.github.com>
2023-10-24 13:01:04 -0400

    Time: 51 ms (49.27%), Space: 18.6 MB (92.89%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
@@ -1,23 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
+        q = collections.deque()
+        res = []
+        if root:
+            q.append(root)
+        else:
             return []
-        
-        result = []
-        queue = deque([root])
-        
-        while queue:
-            curr_level_size = len(queue)
-            max_val = float('-inf')
-            
-            for _ in range(curr_level_size):
-                node = queue.popleft()
-                max_val = max(max_val, node.val)
-                
-                for child in [node.left, node.right]:
-                    if child:
-                        queue.append(child)
-            
-            result.append(max_val)
-        
-        return result
+
+        while q:
+            subres = []
+            for i in range(len(q)):
+                node = q.popleft()
+                subres.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)        
+            res.append(max(subres))
+        return res if root else []
