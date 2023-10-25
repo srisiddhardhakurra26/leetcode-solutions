97f937fd766653097864181e9aa5a4df4810dcda
Sri Siddhardha Kurra <113198317+srisiddhardhakurra26@users.noreply.github.com>
2023-10-25 15:42:57 -0400

    Time: 116 ms (75.14%), Space: 17 MB (55.42%) - LeetHub

--- 1380-number-of-closed-islands/1380-number-of-closed-islands.py
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.py
@@ -1,19 +1,26 @@
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        visit = set()
-        res = 0
 
         def dfs(r, c):
-            if r < 0 or c < 0 or r == rows or c == cols:
-                return 0
-            if grid[r][c] == 1 or (r, c) in visit:
-                return 1
-            visit.add((r, c))
-            return min(dfs(r + 1, c), dfs(r - 1, c), dfs(r, c + 1), dfs(r, c - 1))
-        
+            if r < 0 or c < 0 or r == rows or c == cols or grid[r][c] == 1:
+                return
+            grid[r][c] = 1
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+        res = 0
+        for r in range(rows):
+            dfs(r, 0)
+            dfs(r, cols - 1)
+        for c in range(cols):
+            dfs(0, c)
+            dfs(rows - 1, c)
         for r in range(rows):
             for c in range(cols):
-                if not grid[r][c] and (r, c) not in visit:
-                    res += dfs(r, c)
+                if grid[r][c] == 0:
+                    dfs(r, c)
+                    res += 1
         return res
