6d30a99ff75389eb521f874b9b3d6c879d342c0f
Sri Siddhardha Kurra <113198317+srisiddhardhakurra26@users.noreply.github.com>
2023-11-12 20:19:40 -0500

    Time: 158 ms (77.18%), Space: 16.8 MB (57.15%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,6 +1,6 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        odd = [0] * len(graph)
+        odd = [0] * len(graph) #odd = 1, even = -1
 
         def bfs(i):
             if odd[i]:
@@ -10,11 +10,11 @@ class Solution:
             while q:
                 i = q.popleft()
                 for nei in graph[i]:
-                    if odd[i] == odd[nei]:
+                    if odd[nei] and odd[i] == odd[nei]:
                         return False
                     elif not odd[nei]:
-                        q.append(nei)
                         odd[nei] = -1 * odd[i]
+                        q.append(nei)
             return True
         for i in range(len(graph)):
             if not bfs(i):
