8b3ea007923935811c498bd1919c4956fe776dfa
Sri Siddhardha Kurra <113198317+srisiddhardhakurra26@users.noreply.github.com>
2024-11-10 14:30:30 -0500

    Time: 4 ms (69.23%), Space: 18.4 MB (45%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,22 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        hm = {i:[] for i in range(numCourses)}
-        visit = set()
-
+        adjList = defaultdict(list)
         for crs, pre in prerequisites:
-            hm[crs].append(pre)
-        
+            adjList[crs].append(pre)
+        visit = set()
         def dfs(crs):
             if crs in visit:
                 return False
-            if hm[crs] == []:
+            if crs not in adjList:
                 return True
-            
             visit.add(crs)
-            for pre in hm[crs]:
+            for pre in adjList[crs]:
                 if not dfs(pre):
                     return False
             visit.remove(crs)
-            hm[crs] = []
+            adjList[crs] = []
             return True
-        
-        for i in range(numCourses):
-            if not dfs(i):
-                return False
-        return True
+        res = True
+        for n in range(numCourses):
+            res = res and dfs(n)
+        return res
