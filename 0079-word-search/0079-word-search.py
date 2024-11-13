e549a7ed4935b3d45fd831a780d0b13805b58ad2
Sri Siddhardha Kurra <113198317+srisiddhardhakurra26@users.noreply.github.com>
2024-11-13 00:41:01 -0500

    Time: 4099 ms (54.24%), Space: 16.7 MB (24.07%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,20 +2,21 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows, cols = len(board), len(board[0])
         visit = set()
-
         def dfs(r, c, i):
             if i == len(word):
                 return True
-            if (r < 0 or c < 0 or r == rows or c == cols or (r, c) in visit or word[i] != board[r][c]):
+            if (r < 0 or c < 0 or r >= rows or c >= cols or (r, c) in visit or board[r][c] != word[i]):
                 return False
-            
             visit.add((r, c))
-            res = dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1)
+            res = (dfs(r + 1, c, i + 1) or
+                    dfs(r - 1, c, i + 1) or
+                    dfs(r, c + 1, i + 1) or
+                    dfs(r, c - 1, i + 1))
             visit.remove((r, c))
             return res
-        
         for r in range(rows):
             for c in range(cols):
-                if dfs(r, c, 0):
-                    return True
+                if word[0] == board[r][c]:
+                    if dfs(r, c, 0):
+                        return True
         return False
