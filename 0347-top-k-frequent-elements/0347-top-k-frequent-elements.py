57f5f24ff321a28331ba5706e14188f85222f971
Sri Siddhardha Kurra <113198317+srisiddhardhakurra26@users.noreply.github.com>
2024-11-12 23:47:14 -0500

    Time: 7 ms (54.93%), Space: 23 MB (11.72%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,13 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         res = []
-        freq = [0] * (len(nums) + 1)
+        freq = {}
         for n in nums:
+            if n not in freq:
+                freq[n] = 0
             freq[n] += 1
         freq2 = [[] for n in range(len(nums) + 1)]
-        for number, frequency in enumerate(freq):
-            if frequency:
-                freq2[frequency].append(number)
+        for key, values in freq.items():
+            freq2[values].append(key)
         for i in range(len(freq2) - 1, -1, -1):
             for val in freq2[i]:
                 res.append(val)
